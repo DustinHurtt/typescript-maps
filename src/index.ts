5043f0ee92c3f0fe3c2b37e3f9e0f77f383b52b7
Dustin Hurtt <dustin_hurtt@outlook.com>
2022-08-28 09:26:48 -0400

    continued Google Maps issues

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,16 @@
 // import { User } from './User';
 // import { Company } from './Company';
 
+if (document.getElementById("map")) {
+    console.log("MAP")
+}
+
 new google.maps.Map(document.getElementById("map") as HTMLElement, {
     zoom: 5,
     center: {
         lat: 0,
         lng: 0
     }
-},
+}
 );
 
